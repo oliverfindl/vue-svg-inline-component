c21331723b9ae4d9bf4fe9d1d82abbaba46fe4c2
Oliver Findl <github@findl.sk>
2021-02-23 19:03:09 +0100

    Updated Vite example

--- examples/vite/src/App.vue
+++ examples/vite/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<svg-inline v-if="source" :source="source" :tag="null" :attributes="{ class: 'class-from-props' }" :cache="cache" :transform-function="transformFunction" :transform-function-options="transformFunctionOptions" :emitUpdates="true" :emitErrors="true" @update="onUpdate" @error="onError" />
+	<svg-inline v-if="source" :source="source" :tag="null" :attributes="{ class: 'class-from-props counter-from-props-' + counter }" :cache="cache" :transform-function="transformFunction" :transform-function-options="transformFunctionOptions" :emitUpdates="true" :emitErrors="true" @update="onUpdate" @error="onError" />
 	<HelloWorld message="Hello Vue 3 + Vite" />
 </template>
 
@@ -8,15 +8,15 @@
 
 	import { ref, onMounted } from "vue";
 	import HelloWorld from "./components/HelloWorld.vue";
-	
+
 	const logos = [ "vuejs/art/master/logo.svg", "FortAwesome/Font-Awesome/master/svgs/brands/vuejs.svg" ].map(svg => `https://raw.githubusercontent.com/${svg}`);
 
 	const counter = ref(0);
 	const source = ref(logos[counter.value]);
 	const cache = ref(false);
 
-	const transformFunction = (svg, options, props) => svg.replace(/(<svg)/, `$1 class="svg-icon counter-${counter.value} class-from-transform-function ${options.class} ${props.attributes.class}"`).replace(/<!--.*?-->/g, "");
-	const transformFunctionOptions = { class: "class-from-transform-function-options" };
+	const transformFunction = (svg, options, props) => svg.replace(/(<svg)/, `$1 class="svg-icon class-from-transform-function ${options.class} counter-from-transform-function-${counter.value} ${props.attributes.class}"`).replace(/<!--.*?-->/g, "");
+	const transformFunctionOptions = { class: `class-from-transform-function-options counter-from-transform-function-options-${counter.value}` };
 
 	const onUpdate = svg => console.log("Logging update:", svg);
 	const onError = error => console.error("Logging error:", error);
@@ -24,9 +24,9 @@
 	onMounted(() => setInterval(() => {
 		source.value = logos[(counter.value % 2 === 0) | 0];
 		cache.value = counter.value > 3;
-		console.log("Counter:", counter.value);
-		console.log("Source:", source.value);
-		console.log("Cache enabled:", cache.value);
+		// console.log("Counter:", counter.value);
+		// console.log("Source:", source.value);
+		// console.log("Cache enabled:", cache.value);
 		counter.value++;
 	}, 1e3));
 
